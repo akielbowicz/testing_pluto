8bd04f700bb130265ff6280fc366c73a59701aa7
akielbowicz <augusto.kiel@gmail.com>
2021-02-26 09:11:32 -0300

    Fix missing dependencies

--- test/test_notebooks_execution.jl
+++ test/test_notebooks_execution.jl
@@ -1,7 +1,9 @@
 using Test
 
 @testset "Notebooks execution" begin
-    paths = [ joinpath(a,d) for (a,b,c) in walkdir("../notebooks") if length(b)==0 for d in c if occursin(".jl",d)]
+    notebooks_path = joinpath("..","notebooks")
+    println(notebooks_path) 
+    paths = [ joinpath(a,d) for (a,b,c) in walkdir(notebooks_path) if length(b)==0 for d in c if occursin(".jl",d)]
     @test length(paths) > 0
     for path in paths
         @test include(path)
