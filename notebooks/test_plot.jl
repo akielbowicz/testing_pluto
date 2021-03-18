d7839a670eee64b269a03df0f2758b840d73ba71
akielbowicz <30580760+akielbowicz@users.noreply.github.com>
2021-03-18 14:08:57 +0000

    Deploying to franklin-output from  @ b2796f605e9ce78910bdbf435ac162cceca9f958 🚀

---
+++ notebooks/test_plot.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ffca0570-7779-11eb-274e-31e8a9434b8f
+using Plots
+
+# ╔═╡ 072e6a90-777a-11eb-18bd-6533760eae8c
+plot(x->x^2)
+
+# ╔═╡ Cell order:
+# ╠═ffca0570-7779-11eb-274e-31e8a9434b8f
+# ╠═072e6a90-777a-11eb-18bd-6533760eae8c
